9115ade086462683c3adc4b520748e64533d9d20
Dolev Levy <Dolevlevy97@gmail.com>
2024-07-18 19:32:12 +0300

    Add header menu open logic

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,17 +1,58 @@
 import { Link, NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+import { useEffect, useState } from 'react'
 
 export function AppHeader() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
+	const [menuToOpen, setMenuToOpen] = useState(null)
+
 	const navigate = useNavigate()
+
+	function handleMenuChange(menu) {
+		if (menuToOpen === menu) {
+			setIsMenuOpen(!isMenuOpen)
+		} else {
+			setIsMenuOpen(true)
+			setMenuToOpen(menu)
+		}
+	}
 
 	return (
 		<header className="app-header full">
-			<NavLink to="/">
-				<div className='logo-wrapper'>
-					<div className='logo'></div>
-				</div>
-			</NavLink>
+			<div className='flex'>
+				<NavLink to="/">
+					<div className='logo-wrapper'>
+						<div className='logo'></div>
+					</div>
+				</NavLink>
+				<section className="header-links">
+					<article className='logo-wrapper' onClick={() => {
+						handleMenuChange('Boards')
+					}}>
+						<p className='header-link-text' title='Boards'>Boards</p>
+						<img src="../src\assets\styles\imgs\Icones\arrow-down.svg" className='svg-arrow-down' alt="arrow-down" />
+					</article>
+					<article className='logo-wrapper' onClick={() => {
+						handleMenuChange('Recent')
+					}}>
+						<p className='header-link-text' title='Recent'>Recent</p>
+						<img src="../src\assets\styles\imgs\Icones\arrow-down.svg" className='svg-arrow-down' alt="arrow-down" />
+					</article>
+					<article className='logo-wrapper' onClick={() => {
+						handleMenuChange('Starred')
+					}}>
+						<p className='header-link-text' title='Starred'>Starred</p>
+						<img src="../src\assets\styles\imgs\Icones\arrow-down.svg" className='svg-arrow-down' alt="arrow-down" />
+					</article>
+					<article className='logo-wrapper' onClick={() => {
+						handleMenuChange('Templates')
+					}}>
+						<p className='header-link-text' title='Templates'>Templates</p>
+						<img src="../src\assets\styles\imgs\Icones\arrow-down.svg" className='svg-arrow-down' alt="arrow-down" />
+					</article>
+				</section>
+			</div>
 			<nav>
 				<Link to={'/board'}>Board</Link>
 			</nav>
