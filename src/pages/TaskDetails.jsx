fd7bfbd9f50b8e9d4f95f06c134450e9e28ed232
Jonathan Dolan <yonidolan1@gmail.com>
2024-07-18 11:44:23 +0300

    Add TaskDetails

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -6,10 +6,8 @@ import { Link, Outlet, useNavigate } from 'react-router-dom';
 export function TaskDetails() {
 
     return (
-        <section className="TaskDetails-container">
-            {/* <h2><Link to={`/board/${board._id}`}> T</Link></h2> */}
-
-            <h1>Test</h1>
+        <section className="task-details-backdrop">
+           Backdrop
         </section >
     )
 }
