c7213dc96972542ccc69b777deff24ed4074357f
Dolev Levy <Dolevlevy97@gmail.com>
2024-07-20 16:00:48 +0300

    Some more homepage progress

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 export function HomePage() {
     return (
         <section className="homepage-container">
-            <section className="homepage-top">
+            <section className="homepage-first">
                 <div className="container">
                     <div className="inner-container">
                         <div className="left-container">
                             <div className="text-block">
                                 <div>
-                                    <h1>Trello brings all your tasks, teammates, and tools together</h1>
+                                    <h1>Trellife brings all your tasks, teammates, and tools together</h1>
                                     <p>Keep everything in the same place—even if your team isn’t.</p>
                                 </div>
                                 <div className="spacer"></div>
@@ -19,6 +19,21 @@ export function HomePage() {
                         </div>
                         <div className="right-container">
                             <img src="../../../src/assets/imgs/homepage.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="homepage-second">
+                <div className="header">
+                    <div className="inner-header">
+                        <div className="text-block">
+                            <div className="text-title">
+                                <p className="trellife-101">Trellife 101</p>
+                                <p className="subtitle">A productivity powerhouse</p>
+                            </div>
+                            <div className="text-content">
+                                <p>Simple, flexible, and powerful. All it takes are boards, lists, and cards to get a clear view of who’s doing what and what needs to get done.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
