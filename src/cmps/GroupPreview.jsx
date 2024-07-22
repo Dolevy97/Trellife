2df1dd48b7051f6ef982eeca94996573eac553a1
Dolev Levy <Dolevlevy97@gmail.com>
2024-07-22 16:35:27 +0300

    Fix task timer container class name

--- src/cmps/GroupPreview.jsx
+++ src/cmps/GroupPreview.jsx
@@ -176,7 +176,7 @@ export function GroupPreview({ group, boardId }) {
                                         {task.dueDate && (
                                             <div
                                                 title={task.isDone ? 'This task is complete.' : 'This task is due later.'}
-                                                className="task-bottom-container"
+                                                className="task-timer-container"
                                                 style={!task.isDone ? {} : { backgroundColor: '#4BCE97' }}
                                             >
                                                 <img
