cd0f6996ed6ec1487dc2b94ce0e24fd6115e546c
Jonathan Dolan <yonidolan1@gmail.com>
2024-07-24 22:31:58 +0300

    Cleanup

--- src/pages/BoardDetails.jsx
+++ src/pages/BoardDetails.jsx
@@ -12,17 +12,19 @@ import { SET_USER } from '../store/reducers/user.reducer.js'
 import { socketService } from '../services/socket.service.js'
 
 export function BoardDetails() {
-  const { boardId } = useParams()
+  
   const user = useSelector(storeState => storeState.userModule.user)
   const board = useSelector(storeState => storeState.boardModule.board)
+
   const [isAddingGroup, setIsAddingGroup] = useState(false)
   const [newGroupTitle, setNewGroupTitle] = useState('')
+  const [areLabelsExpanded, setAreLabelsExpanded] = useState(false)
+  
   const groupListContainer = useRef()
   const groupListHeader = useRef()
-  const [areLabelsExpanded, setAreLabelsExpanded] = useState(false)
-
   const addGroupRef = useRef(null)
-
+  
+  const { boardId } = useParams()
 
   useEffect(() => {
     loadBoard(boardId)
@@ -39,8 +41,6 @@ export function BoardDetails() {
       socketService.login(guest._id)
     }
   }, [user])
-
-  console.log(user)
 
   useEffect(() => {
     if (board && board.style && board.style.background && groupListContainer) {
