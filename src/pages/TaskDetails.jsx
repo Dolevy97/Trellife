b21ab8d8a79a1fdf45eb40ce33efc767d2db5633
Jonathan Dolan <yonidolan1@gmail.com>
2024-07-25 00:08:23 +0300

    Style TaskDetails

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -807,7 +807,7 @@ export function TaskDetails() {
 
                         <button className="action remove-task" onClick={onRemoveTask}>
                             <img className="icon" src="../../../src/assets/imgs/TaskDetails-icons/trash.svg" alt="close icon" />
-                            <span className="action-title">Remove card</span>
+                            <span className="action-title">Delete card</span>
                         </button>
 
                     </section>
