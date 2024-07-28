f75661aa874bb210519060733c81ba57c03f7391
Yonatan Hershko <yonher8@gmail.com>
2024-07-28 14:41:40 +0300

    Add all quick edit functions

--- src/cmps/GroupPreview.jsx
+++ src/cmps/GroupPreview.jsx
@@ -248,6 +248,7 @@ export function GroupPreview({ group, boardId, handleOnDragEnd, toggleLabelExpan
                                                             group={group}
                                                             board={board}
                                                             user={user}
+                                                            handleTaskClick={handleTaskClick}
                                                         />
                                                     )}
                                                     <div
