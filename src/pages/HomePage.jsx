7242bfc398c41fa83f848a16a9622570af7c0e13
Jonathan Dolan <yonidolan1@gmail.com>
2024-07-28 09:50:25 +0300

    Add set for Home Page carousel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -54,6 +54,27 @@ export function HomePage() {
                         </div>
                     </div>
                 </div>
+                {/* <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="../../../src/assets/imgs/board-details-screenshot.png" class="d-block w-100" alt="..." />
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../../../src/assets/imgs/drag-and-drop-screenshot.png" class="d-block w-100" alt="..." />
+                        </div>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="..."/>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div> */}
             </section>
         </section >
     )
